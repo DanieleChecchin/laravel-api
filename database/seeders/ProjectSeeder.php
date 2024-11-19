f46301d072548dccdb9e6515bdad796314e7822a
DanieleChecchin <danielechecchin26@gmail.com>
2024-11-19 16:51:58 +0100

    added projects in project seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -17,12 +17,33 @@ class ProjectSeeder extends Seeder
     {
         $typeIds = Type::all()->pluck('id'); //prendo tutti i types
 
-        $projectNames=[
+        $projectNames = [
             'boolflix',
             'boolando',
             'boolzap',
             'db_university',
-            'hello_world'
+            'hello_world',
+            'vite-boolflix',
+            'vite-comics',
+            'vite-yu-gi-oh',
+            'html-london-trip',
+            'js-biglietto-treno',
+            'js-simon',
+            'php-hotel',
+            'laravel-api',
+            'laravel-comics',
+            'js-our-team',
+            'js-fizzbuzzdom',
+            'vue-todolist',
+            'db-university',
+            'html-css-boolando',
+            'js-paliedispari',
+            'php-badwords',
+            'htmlcss-discord',
+            'db-first',
+            'htmlcss-shoes',
+            'vue-email-list',
+            'html-css-flukeout',
         ];
         foreach ($projectNames as $name) {
             $newProject = new Project();
